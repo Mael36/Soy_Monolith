b4a1a061eb34425884be65628df5352ae6012152
SSB Personne <ssbpersonne@gmail.com>
2026-05-19 14:36:12 +0200

    fix(db): make postgres ssl configurable via env variable

--- back/config/ConfDB.js
+++ back/config/ConfDB.js
@@ -1,7 +1,14 @@
+const sslEnabled =
+  process.env.POSTGRES_SSL === "true" ||
+  process.env.POSTGRES_SSL === true;
+
 module.exports = {
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   host: process.env.POSTGRES_HOST,
-  ssl: false
+
+  ssl: sslEnabled
+    ? { rejectUnauthorized: false }
+    : false
 };
